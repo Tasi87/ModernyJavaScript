4deb92453381176eed0312fa4b086d408309766f
Tasi <tasi87@gmail.com>
2025-06-07 11:28:38 +0200

    poznámky k zameriavaniu a vyberaniu elementov

--- js/script_advanced.js
+++ js/script_advanced.js
@@ -1,5 +1,36 @@
+	console.log("%cVyberanie elementov pomocou JavaScriptu:", "color: red; font-weight: bold")
+	
+	// querySelector
+	//note keď chcem pomocou JS vybrať jednotlivý element
+	//! vždy vyberie IBA PRVÚ možnosť
+	const paragraph = document.querySelector("p")
+	console.log(paragraph)
+	
+	// querySelectorAll
+	//note pracuje sa s ním ako s poliami
+	//! vyberie všetky prvky
+	let allParagraphs = document.querySelectorAll("p")
+	console.log(allParagraphs)
+	// console.log(allParagraphs[0])
+	// console.log(allParagraphs[1])
+	// console.log(allParagraphs[2])
+	
+	// getElementById
+	let myId = document.getElementById("DOOM")
+	console.log(myId)
+	
+	// getElementByClassName
+	//note tiež pracuje ako s poliami
+	let myClass = document.getElementsByClassName("para")
+	console.log(myClass)
+	// console.log(myClass[0])
+	// console.log(myClass[1])
+	
+	//------------------------------------------------------------------------------------
+	console.log("%cVyberanie elementov pomocou JavaScriptu:", "color: red; font-weight: bold")
 
-//------------------------------------------------------------------------------------
+
+	
 //------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------
