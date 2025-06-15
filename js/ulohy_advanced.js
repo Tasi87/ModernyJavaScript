fc7767246ba6f45837b846c890baede83545c413
Tasi <tasi87@gmail.com>
2025-06-15 12:09:40 +0200

    praca s filtrom

--- js/ulohy_advanced.js
+++ js/ulohy_advanced.js
@@ -49,38 +49,106 @@ V html vytvorte nadpisy h1, h2, h3. Vo všetkých bude ľubovolný text. U všet
 */
 
 // Vytvorenie h elementov
-const creatH1 = document.createElement("h1")
-creatH1.textContent = "Sem KLIKNI!"
-document.querySelector("section").appendChild(creatH1)
+// const creatH1 = document.createElement("h1")
+// creatH1.textContent = "Sem KLIKNI!"
+// document.querySelector("section").appendChild(creatH1)
 
-const creatH2 = document.createElement("h2")
-creatH2.textContent = "Sem KLIKNI!"
-document.querySelector("section").appendChild(creatH2)
+// const creatH2 = document.createElement("h2")
+// creatH2.textContent = "Sem KLIKNI!"
+// document.querySelector("section").appendChild(creatH2)
 
-const creatH3 = document.createElement("h3")
-creatH3.textContent = "Sem KLIKNI!"
-document.querySelector("section").appendChild(creatH3)
+// const creatH3 = document.createElement("h3")
+// creatH3.textContent = "Sem KLIKNI!"
+// document.querySelector("section").appendChild(creatH3)
 
-// Funkcia na kliknutie (call back function)
+// // Funkcia na kliknutie (call back function)
 
-document.querySelector("section").querySelector("h1").addEventListener("click", function (event) {
-	event.target.textContent = "Klikni na nadpis nižšie"
+// document.querySelector("section").querySelector("h1").addEventListener("click", function (event) {
+// 	event.target.textContent = "Klikni na nadpis nižšie"
+// })
+
+// document.querySelector("section").querySelector("h2").addEventListener("click", function (event) {
+// 	event.target.textContent = "Klikni na nadpis nižšie"
+// })
+
+// document.querySelector("section").querySelector("h3").addEventListener("click", function (event) {
+// 	event.target.textContent = "Klikni na nadpis nižšie"
+// })
+
+
+//------------------------------------------------------------------------------------
+console.log("%c Úloha 3:", "color: red; font-weight: bold")
+
+let criminals = [{
+	firstName: "Martin",
+	secondName: "Zelený",
+	birth: 1985,
+	licencePlate: "85C3322",
+	address: "U sloupů 16",
+	city: "České Budějovice"
+}, {
+	firstName: "Jana",
+	secondName: "Růžová",
+	birth: 1996,
+	licencePlate: "93K3922",
+	address: "Malská 29",
+	city: "České Budějovice"
+}, {
+	firstName: "Anna",
+	secondName: "Modrá",
+	birth: 1989,
+	licencePlate: "2EP6328",
+	address: "Stevardská 38",
+	city: "České Budějovice"
+}]
+
+// Uloženie dát z Poľa
+let filters = {
+	//note tu zadáme hľadaný text
+	searchText: ""
+}
+
+// Filter
+											//! ourCriminals a tryToFind môžu byť akékoľvek názvy
+const renderCriminals = function (ourCriminals, tryToFind) {
+	//note do "arrayResult" ukladám výsledky, ktoré odpovedajú tomu, čo vpisujem do "input"
+	let arrayResult = ourCriminals.filter(function (oneSuspect) {
+		//note pýtam sa "includes"(zahrňuje) to, čo máme v tryToFind, ale ten text máme v "searchText", preto zadávam "inclueds(tryToFind.searchText.toLowerCase())"
+		return oneSuspect.licencePlate.toLowerCase().includes(tryToFind.searchText.toLowerCase())
+	})
+
+// Premazávanie nepotrebných výsledkov
+document.querySelector("#idCriminal").innerHTML = ""
+
+// Vypisovanie výsledku
+arrayResult.forEach(function (oneSuspect) {
+	let para = document.createElement("p")
+	//note ".innerHTML" bude brať ako html a nebude vypisovať <br> ako pri ".textContent"
+	para.innerHTML = `
+	Meno: ${oneSuspect.firstName} <br>
+	Priezvisko: ${oneSuspect.secondName} <br>
+	Rok narodenia: ${oneSuspect.birth} <br>
+	ŠPZ: ${oneSuspect.licencePlate} <br>
+	Adresa: ${oneSuspect.address} <br>
+	Mesto: ${oneSuspect.city}
+	`
+	document.querySelector("#idCriminal").appendChild(para)
 })
 
-document.querySelector("section").querySelector("h2").addEventListener("click", function (event) {
-	event.target.textContent = "Klikni na nadpis nižšie"
+}
+
+// Načítanie dát z Poľa
+let licencePlate = document.querySelector("#licence-plate")
+
+licencePlate.addEventListener("input", function (event) {
+	//note vpisovaný text do inputu ukladám do "filters" a do "searchText"
+	filters.searchText = event.target.value
+	
+	renderCriminals(criminals, filters)
 })
 
-document.querySelector("section").querySelector("h3").addEventListener("click", function (event) {
-	event.target.textContent = "Klikni na nadpis nižšie"
-})
-
-
 //------------------------------------------------------------------------------------
-console.log("%c Úloha 21:", "color: red; font-weight: bold")
-
-//------------------------------------------------------------------------------------
-console.log("%c Úloha 21:", "color: red; font-weight: bold")
+// console.log("%c Úloha 21:", "color: red; font-weight: bold")
 
 //------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------
@@ -92,3 +160,19 @@ console.log("%c Úloha 21:", "color: red; font-weight: bold")
 //------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------
+	
+//! Odstránenie prebytočného obsahu
+	// Odstránenie všetkých "p"
+	let allParagraphs = document.querySelectorAll("p")
+	allParagraphs.forEach(deleteAllP => {
+		deleteAllP.remove()
+	});
+	// Odstránenie všetkých "h2"
+	let h2 = document.querySelectorAll("h2")
+	h2.forEach(deleteAllH2 => {
+		deleteAllH2.remove()
+	});
+	// Odstránenie input-txt
+	let input = document.querySelector("#input-txt")
+	input.remove()
+
