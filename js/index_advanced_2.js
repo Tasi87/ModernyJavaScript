0964c8b826ebcb9e085df063f8338ea3017304f8
Tasi <tasi87@gmail.com>
2025-06-15 15:04:24 +0200

    praca s formularom

---
+++ js/index_advanced_2.js
@@ -0,0 +1,18 @@
+document.querySelector("#test-form").addEventListener("submit", function (event) {
+	// Vypnutie predvoleného správania formuláru
+	event.preventDefault()
+
+	// Prístup k obsahu "input"
+		//note "event(toto je z funkcie).target(cieľ).elements(je to element).firstName(v HTML som priradil inputu).value(hodnota, na ktorú sa chcem zamerať)"
+	//! kontrola funkčnosti
+	// console.log(event.target.elements.firstName.value)
+
+	// Vytvorenie odstavca a pridanie textu z políčka
+	let para = document.createElement("p")
+	para.textContent = event.target.elements.firstName.value
+
+	document.querySelector("#from_form").appendChild(para)
+
+	// Vymazanie obsahu inputu po odoslaní
+	event.target.elements.firstName.value = ""
+})
