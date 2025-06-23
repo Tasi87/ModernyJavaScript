ac9fae81dec06ea4f11dc4b97b841dde881180e1
Tasi <tasi87@gmail.com>
2025-06-23 15:56:06 +0200

    scrollovacia ikonka

--- js/index_advanced_5.js
+++ js/index_advanced_5.js
@@ -1,10 +1,37 @@
+// window.addEventListener("scroll", function () {
+// 	// variables
+// 	let scrolled = window.scrollY
+// 	let scrollable = document.documentElement.scrollHeight - window.innerHeight
+// 	//note "inner.Height" je vnútorná časť webu, to pod záložkami, kde sa mi načíta obsah stránky
+
+// 	if (Math.floor(scrolled) === scrollable) {
+// 		alert("Si na KONCI!!!")
+// 	}
+// })
+
+// 		!!SCROLLOVACIA IKONKA!!!
+
 window.addEventListener("scroll", function () {
 	// variables
 	let scrolled = window.scrollY
 	let scrollable = document.documentElement.scrollHeight - window.innerHeight
-	//note "inner.Height" je vnútorná časť webu, to pod záložkami, kde sa mi načíta obsah stránky
 
-	if (Math.floor(scrolled) === scrollable) {
-		alert("Si na KONCI!!!")
+	// if (Math.floor(scrolled) === scrollable) {
+	// 	let toTop = document.querySelector(".top-page")
+	// 	toTop.style.display = "block"
+
+	// 	toTop.addEventListener("click", function () {
+	// 		toTop.style.display = "none"
+	// 	})
+	// }
+
+	if (Math.floor(scrolled) > 50) {
+		let toTop = document.querySelector(".top-page")
+		toTop.style.display = "block"
+
+		toTop.addEventListener("click", function () {
+			toTop.style.display = "none"
+		})
 	}
+	
 })
