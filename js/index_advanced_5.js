96a49d4db43eb6c3cd3c2840e14eb92ef71ecd26
Tasi <tasi87@gmail.com>
2025-07-21 10:18:46 +0200

    scroll na element

--- js/index_advanced_5.js
+++ js/index_advanced_5.js
@@ -13,25 +13,33 @@
 
 window.addEventListener("scroll", function () {
 	// variables
+	//note "scrolled" vypočítava polohu scrollu na stránke
 	let scrolled = window.scrollY
-	let scrollable = document.documentElement.scrollHeight - window.innerHeight
+	// console.log(scrolled)
+	//note "scrollable" mi pomôže ukázať maximálnu hodnotu scrollu
+	// let scrollable = document.documentElement.scrollHeight - window.innerHeight
+	// console.log(scrollable)
+	
+	if (Math.floor(scrolled) < 50) {
+		let toTop = document.querySelector(".top-page")
+		toTop.style.display = "none"
 
-	// if (Math.floor(scrolled) === scrollable) {
-	// 	let toTop = document.querySelector(".top-page")
-	// 	toTop.style.display = "block"
-
-	// 	toTop.addEventListener("click", function () {
-	// 		toTop.style.display = "none"
-	// 	})
-	// }
-
-	if (Math.floor(scrolled) > 50) {
+		//idea Toto tu už nie je potrebné, pretože som to prerobil,tak, že vždy "window.scrollY" klesne pod 50, tak nastane na šípke hore `display = "none"`
+		// toTop.addEventListener("click", function () {
+		// 	toTop.style.display = "none"
+		// })
+	} else {
 		let toTop = document.querySelector(".top-page")
 		toTop.style.display = "block"
-
-		toTop.addEventListener("click", function () {
-			toTop.style.display = "none"
-		})
 	}
-	
+})
+
+let firstItemMenu = document.querySelector(".first-item-menu")
+let scrollGoal = document.querySelector(".scroll-goal").offsetTop
+
+firstItemMenu.addEventListener("click", function () {
+	window.scrollTo({
+		top: scrollGoal,
+		behavior: "smooth" //! v mozille nefunguje!
+	})
 })
