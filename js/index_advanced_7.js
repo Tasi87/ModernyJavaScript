9ae9d204021ba25956fc935276d4361aef7ea7d9
Tasi <tasi87@gmail.com>
2026-01-19 14:36:57 +0100

    vytvorenie premenej myForm, ktora sleduje vstup do pola a jeho odoslanie

---
+++ js/index_advanced_7.js
@@ -0,0 +1,11 @@
+//note "names" je pole, do ktorého sa ukladajú alebo načítajú data z funkcie "getSavedNames"
+const names = getSavedNames()
+
+let myForm = document.querySelector("#test-form")
+	myForm.addEventListener("submit", function (event) {
+	event.preventDefault()
+
+		names.push(
+
+		)
+})
