cf9f021566def0ea682c7f7a2cc58c22ed750cb8
Tasi <tasi87@gmail.com>
2026-01-21 12:03:11 +0100

    vytvorenie mazacieho btn a prepojenie ho s menami

--- js/functions_advanced_7.js
+++ js/functions_advanced_7.js
@@ -1,18 +1,49 @@
-/*
+/**
 	Funkcia načítajúca data z localStorage;
 	Ošetriť pokiaľ data v localStorage nie sú
 */
 
 const getSavedNames = function () {
-											//note vytiahne data z "localStarage"
+						//note vytiahne data z "localStarage"
 	const myNames = localStorage.getItem("names")
 	
-											//note ak tam nejaké data sú, takže "myNames" sa nerovná "0"
+						//note ak tam nejaké data sú, takže "myNames" sa nerovná "0"
 	if (myNames !== null) {
-											//note rovno to prevedie do poľa objektov
+						//note rovno to prevedie do poľa objektov
 		return JSON.parse(myNames)
 	} else {
-											//note ak sa "myNames" rovná "0", tak vráti prázdne pole
+						//note ak sa "myNames" rovná "0", tak vráti prázdne pole
 		return []
 	}
+}
+
+/**
+	Funkcia pre použitie pri odoslaní formuláru;
+	Ukladá do localStorage meno z formuláru
+ */
+
+const saveNames = function (oneName) {
+						//note prevedie sa na "string" a uloži do "localStorage" pod "names"
+	localStorage.setItem("names", JSON.stringify(oneName))
+}
+
+/**
+	Generovanie HTML štruktúry, ktorú umiestnime do schránky po kliknutí na tlačítko "Vypísať"
+	+ použijeme ju taktiež pre vypísanie nových informácií z localStorage, 
+	keď nejaké meno vymažeme pomocou tlačítka "Vymazať meno"
+*/
+
+const generateHTMLstructure = function (oneName) {
+	const newDiv 	= document.createElement("div")
+	const newSpan 	= document.createElement("span")
+	const button	= document.createElement("button")
+
+	// nastavenie mazacieho tlačítka
+	button.textContent = "Vymazať meno"
+	newDiv.appendChild(button)
+
+	newSpan.textContent = oneName.firstName
+	newDiv.appendChild(newSpan)
+
+	return newDiv
 }
--- js/index_advanced_7.js
+++ js/index_advanced_7.js
@@ -1,6 +1,11 @@
+/** načitanie dát z localStorage do premenej names; pokiaľ je localStorage prázdny, 
+tak do names sa uloží prázdne pole.
+ */
+
 //note "names" je pole, do ktorého sa ukladajú alebo načítajú data z funkcie "getSavedNames"
 const names = getSavedNames()
 
+// odosielanie formuláru a ulokladanie do localStorage pomocou premennej names
 let myForm = document.querySelector("#test-form")
 	myForm.addEventListener("submit", function (event) {
 	event.preventDefault()
@@ -8,9 +13,24 @@ let myForm = document.querySelector("#test-form")
 		//note do poľa "names" pridá nakoniec to, čo sa bude písať do vstupu a pridá sa "id"
 		// konštanta prepojená s funkciou "getSavedNames"
 		names.push({
-			id: "",
+			id: uuidv4(),	//! vytvorí špeciálne "id" pre každé meno uložené do poľa
 			firstName: event.target.elements.firstName.value
 		})
+		// vymaže input po submitef
+		event.target.elements.firstName.value = ""
 
+		saveNames(names)
+})
 
+// vypisovanie späť do schránky
+let btnToList = document.querySelector(".to-list")
+btnToList.addEventListener("click", function (event) {
+	
+	let namesFromStorage = localStorage.getItem("names")
+	let namesFromStorageJSON = JSON.parse(namesFromStorage)
+	
+	namesFromStorageJSON.forEach(function (myName) {
+		const oneNameHTML = generateHTMLstructure(myName)
+		document.querySelector(".list-names").appendChild(oneNameHTML)
+	})
 })
