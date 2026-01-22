b8c711d80fc672c75dda4a7cd502b04f69fc70f3
Tasi <tasi87@gmail.com>
2026-01-22 15:39:54 +0100

    vytvorenie moznosti editovania mena a jeho ulezenie do localStorage a linku na vratenie na domovsku stranku

--- js/index_advanced_7_edit.js
+++ js/index_advanced_7_edit.js
@@ -14,3 +14,20 @@ if(searchedName === undefined) {
 	// ... tohoto bude presmerovaný na pôvodnú stránku
 	location.assign("../index_advances_7.html")
 }
+
+// toto mi vypíše meno do inputu
+document.querySelector("#edited-name").value = searchedName.firstName
+
+// vytvorí premenu, ktorá vyberie element s id "changing-form"
+let changingForm = document.querySelector("#changing-form")
+// na premenej sledujeme "submit" a spúšťa funkciu
+changingForm.addEventListener("submit", function (event) {
+	event.preventDefault()
+
+	// zmení meno po submite
+	searchedName.firstName = event.target.elements.changingName.value
+
+	//note uloží zmenu do konštanty "names"
+	saveNames(names)
+	
+})
