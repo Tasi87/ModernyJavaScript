ca41bf16c6292da61351c3366cca828f69fbb75e
Tasi <tasi87@gmail.com>
2026-02-05 11:19:12 +0100

    precvicovanie sipkovej notacie v poli

--- lekcia_8/script.js
+++ lekcia_8/script.js
@@ -25,3 +25,43 @@
 
 	console.log(diff(6,2))
 	console.log(diff(10,12))
+
+//---------------------------------------------------------------------------
+//idea Šípková notácia v poli
+
+const people = [{
+	name: "Tasi",
+	age: 38
+}, {
+	name: "Harry",
+	age: 15
+},{
+	name: "Ron",
+	age: 18
+}]
+
+// const less20 = people.filter(function (onePerson) {
+// 	return onePerson.age < 20
+// })
+
+const less20 = people.filter((onePerson) => onePerson.age < 20)
+console.log(less20)
+
+// 1. nájdite človeka, kt. presne  15
+	const age15 = people.filter((young) => young.age == 15)
+	console.log(age15)
+
+// 2. nájdite človeka, kt. sa volá Ron
+	const exactName = people.filter((person) => person.name == "Ron")
+	console.log(exactName)
+
+// 3. ukázať rozdiel medzi filtrami
+	//! !!!	rozdiel medzi "find" a "filter" je ten, že "find" nájde, a zobrazí prvý výsledok,
+	//!		na ktorý narazí a "filter", zobrazí všetký výsledky	!!!
+	/**
+		rozdiel je v použití filtra, kde v prvom prípade filtrujem podľa "age"
+		a v druhom prípade filtrujem podľa "name", pričom v druhom prípade
+		pracujem so STRINGOM a podľa toho aj postupovať, string sa označuje ""
+	*/ 
+//---------------------------------------------------------------------------
+//---------------------------------------------------------------------------
