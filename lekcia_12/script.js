45ed75e2301545f0d236c1690381b5370ec7f3bc
Tasi <tasi87@gmail.com>
2026-02-10 08:46:30 +0100

    poznámky ohľadom cyklov while a do while

---
+++ lekcia_12/script.js
@@ -0,0 +1,24 @@
+//idea	Najčastejšie opakované cykly
+//------------------------------------------------------------------------------------------
+//note	forEach - prechádza polia
+//note	for - poznáme počet opakovaní tzn. pokiaľ vieme, ze sa niečo má opakovatnapr.: 10x
+//note	while	(pokiaľ) - nemusí prebehnúť ani raz
+//note	do while  - prebehne vždy minimálne raz
+//------------------------------------------------------------------------------------------
+let i = 20
+
+	// while (i < 10) {
+	// 	console.log(i)
+	// 	//note i-- je i = i - 1
+	// 	//note i++ je i = i + 1
+	// 	i++
+	// }
+// while prebehne iba ak je podmienka splnená, //!	!!!NEMUSÍ PREBEHNÚ%T ANI RAZ!!!
+
+	// do {
+	// 	console.log(i)
+	// 	i++
+	// } while (i < 10)
+// cyklus do //! !!!PREBEHNE URČITE ASPOŇ RAZ!!!
+// podmienka while //! !!!PREBEHNE IBA AK JE PODMIENKA SPLNENÁ!!!
+//------------------------------------------------------------------------------------------
